86efa3ca248685949c6becb7cf00c328ab977ae4
Gerardo <fernandez.gerardo@gmail.com>
2024-05-15 23:42:58 -0300

    Remove Zustand and reformed Seach form

---
+++ src/components/FormSearch.tsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react"
+import { typesAccesory, accesories, type Accesory } from "../lib/data";
+
+const typesList = Object.values(typesAccesory);
+
+export const FormSearch = () => {
+  const [search, setSearch] = useState<string>(() => {
+    const searchParams = new URLSearchParams(window.location.search)
+    return searchParams.get('q') ?? ''
+  })
+  const [filterParam, setFilterParam] = useState("all")
+  const [currentaData, setCurrentaData] = useState<Array<Accesory>>(accesories)
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value)
+  }
+
+  const handleSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setFilterParam(event.target.value)
+  }
+
+  // actualizar el link del navegador
+  useEffect(() => {
+    // Mejorable
+    const searchPath = `${search ?? ''}`
+      
+    const filterPath = filterParam === 'all'
+      ? ''
+      : `&f=${filterParam}`
+
+    const newPathName = searchPath || filterPath ? `?q=${searchPath}${filterPath}` : window.location.pathname
+
+    window.history.replaceState({}, '', newPathName)
+
+    // LLamar a la api para filtrar los resultados
+    if (!search && !filterParam){
+      setCurrentaData(accesories)
+      return
+    }
+
+    fetch(`/api/get-info-accesory.json?q=${search}&f=${filterParam}`)
+      .then(res => res.json())
+      .then(data => {
+        const { accesoriesFilter } = data
+        setCurrentaData(accesoriesFilter)
+      })
+  }, [search, filterParam])
+
+  const classSelect = "bg-slate-300/20 text-zinc-800 checked:text-gray-800 checked:bg-slate-300/60"
+
+  console.log(currentaData)
+  return (
+    <form className="flex p-3 justify-center gap-2">
+      <input
+        onChange={handleSearch}
+        type="text"
+        placeholder="Buscar..."
+        name="query"
+        id="search"
+        className="block w-full py-1.5 pr-5 text-gray-700 bg-white border border-gray-200 rounded-lg md:w-80 placeholder-gray-400/70 pl-11 rtl:pr-11 rtl:pl-5 focus:border-blue-400 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+      />
+      <label htmlFor="select"></label>
+      <select
+        id="select"
+        className="bg-zinc-50 border border-slate-300 text-gray-900 text-sm rounded-lg focus:ring focus:ring-opacity-40 focus:ring-blue-300 focus:border-blue-400 focus:outline-none block w-full p-2.5"
+        onChange={handleSelect}
+        value={filterParam}
+      >
+        <option value='all' className={classSelect}>Todos</option>
+        {
+          typesList.map((tipo, index) => (
+            <option value={tipo} className={classSelect} key={index}>
+              {tipo.replace(/^./, (char) => char.toUpperCase())}
+            </option>
+          ))
+        }
+      </select>
+    </form>
+  )
+}
--- src/pages/api/get-info-accesory.json.js
+++ src/pages/api/get-info-accesory.json.js
@@ -7,8 +7,16 @@ export async function GET({ params, request }) {
   const { url } = request
   const urlObject = new URL(url)
   const query = urlObject.searchParams.get('q')
+  const filterParam = urlObject.searchParams.get('f')
 
-  const accesoriesFilter = accesories.filter((accesory) => accesory.nombre.includes(query))
+  const accesoriesFilter = accesories.filter((accesory) => {
+    if ((query && !accesory.nombre.includes(query))
+      || (filterParam && accesory.tipo !== filterParam)
+    ) return
+
+    return accesory
+  })
+  // https://www.youtube.com/watch?v=wSxIwy6GvT4
 
   return new Response(JSON.stringify({accesoriesFilter}), {
     headers: { "content-type": "application/json" }
