41f08d0b43407f0096b9c6542a19adcaa7ea884c
Gerardo <fernandez.gerardo@gmail.com>
2024-06-04 00:15:38 -0300

    Add filter category and styles in components

--- README.md
+++ README.md
@@ -1,54 +1,29 @@
-# Astro Starter Kit: Basics
+# Contro de Stock
+
+## Astro + React + TypeScript + Nanostores
 
 ```sh
-npm create astro@latest -- --template basics
+cd ./stock-panel/
+pnpm install
+pnpm run dev
 ```
 
-[![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz.svg)](https://stackblitz.com/github/withastro/astro/tree/latest/examples/basics)
-[![Open with CodeSandbox](https://assets.codesandbox.io/github/button-edit-lime.svg)](https://codesandbox.io/p/sandbox/github/withastro/astro/tree/latest/examples/basics)
-[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/withastro/astro?devcontainer_path=.devcontainer/basics/devcontainer.json)
+![alt text](image.png)
 
-> 🧑‍🚀 **Seasoned astronaut?** Delete this file. Have fun!
+## Acerca del proyecto
 
-![just-the-basics](https://github.com/withastro/astro/assets/2244813/a0a5533c-a856-4198-8470-2d67b1d7c554)
+Aplicación web que gestiona un control del inventario de accesorios.
+Tiene la opción de buscar por "Nombre" y filtrar por "Tipo".
 
-## 🚀 Project Structure
+## Faltante
 
-Inside of your Astro project, you'll see the following folders and files:
-
-```text
-/
-├── public/
-│   └── favicon.svg
-├── src/
-│   ├── components/
-│   │   └── Card.astro
-│   ├── layouts/
-│   │   └── Layout.astro
-│   └── pages/
-│       └── index.astro
-└── package.json
-```
-
-Astro looks for `.astro` or `.md` files in the `src/pages/` directory. Each page is exposed as a route based on its file name.
-
-There's nothing special about `src/components/`, but that's where we like to put any Astro/React/Vue/Svelte/Preact components.
-
-Any static assets, like images, can be placed in the `public/` directory.
-
-## 🧞 Commands
-
-All commands are run from the root of the project, from a terminal:
-
-| Command                   | Action                                           |
-| :------------------------ | :----------------------------------------------- |
-| `npm install`             | Installs dependencies                            |
-| `npm run dev`             | Starts local dev server at `localhost:4321`      |
-| `npm run build`           | Build your production site to `./dist/`          |
-| `npm run preview`         | Preview your build locally, before deploying     |
-| `npm run astro ...`       | Run CLI commands like `astro add`, `astro check` |
-| `npm run astro -- --help` | Get help using the Astro CLI                     |
-
-## 👀 Want to learn more?
-
-Feel free to check [our documentation](https://docs.astro.build) or jump into our [Discord server](https://astro.build/chat).
+- Buscar por la columna "categoría".
+  * Selección simple.
+  * Selección multiple
+- Abstraer componentes
+  * Filter...
+- Hacer una busqueda multiple con "nombre" y "categoría".
+- Hace que funcione los botones de "Editar" y "Eliminar".
+- Hacer responsive la Tabla.
+- Agregar estilos y reposisionamiento cuando un accesorio es eliminado.
+- Agregar estilos a "Estado".
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -5,6 +5,22 @@ export const typesAccesory = {
   HERRAMIENTA: 'herramienta'
 }
 
+export const categoriesAccesory = {
+  45: '45°',
+  FUSION: 'fusion',
+  HH: 'h-h',
+  INJERTO: 'injerto metalico',
+  MH: 'm-h',
+  ROJO: 'rojo',
+  ROSCA: 'rosca',
+  VERDE: 'verde'
+}
+
+const stateAccesory = {
+  OK: 'ok',
+  REPARAR: 'reparar'
+}
+
 // type typeAccesory = keyof typeof typesAccesory
 
 export interface Accesory {
@@ -26,10 +42,10 @@ export const accesories: Accesories = [
     id: '1',
     nombre: 'codo',
     tipo: typesAccesory.AGUA,
-    categoria: ['rojo', 'rosca', '45°', 'h-h'],
+    categoria: [categoriesAccesory.ROJO, categoriesAccesory.ROSCA, categoriesAccesory[45], categoriesAccesory.HH],
     tamanio: '1/2',
     cantidad: 7,
-    estado: 'ok',
+    estado: stateAccesory.OK,
     imagen: '',
     isDeleted: false
   },
@@ -37,10 +53,10 @@ export const accesories: Accesories = [
     id: '2',
     nombre: 'cupla',
     tipo: typesAccesory.AGUA,
-    categoria: ['rojo', 'fusion', 'h-h'],
+    categoria: [categoriesAccesory.ROJO, categoriesAccesory.FUSION, categoriesAccesory.HH],
     tamanio: '3/4',
     cantidad: 3,
-    estado: 'ok',
+    estado: stateAccesory.OK,
     imagen: '',
     isDeleted: false
   },
@@ -48,10 +64,10 @@ export const accesories: Accesories = [
     id: '3',
     nombre: 'cupla',
     tipo: typesAccesory.GAS,
-    categoria: ['fusion', 'injerto metalico', 'h-m'],
+    categoria: [categoriesAccesory.FUSION, categoriesAccesory.INJERTO, categoriesAccesory.MH],
     tamanio: '25 x 1/2',
     cantidad: 1,
-    estado: 'acondicionar',
+    estado: stateAccesory.REPARAR,
     imagen: '',
     isDeleted: false
   },
@@ -59,21 +75,21 @@ export const accesories: Accesories = [
     id: '4',
     nombre: 'curva',
     tipo: typesAccesory.AGUA,
-    categoria: ['verde', 'fusion', 'h-h'],
+    categoria: [categoriesAccesory.VERDE, categoriesAccesory.FUSION, categoriesAccesory.HH],
     tamanio: '20',
     cantidad: 7,
-    estado: 'ok',
+    estado: stateAccesory.OK,
     imagen: '',
     isDeleted: false
   },
   {
     id: '5',
-    nombre: 'sobrepaso',
+    nombre: 'llave de paso',
     tipo: typesAccesory.AGUA,
-    categoria: ['verde', 'fusion', 'h-h'],
+    categoria: [categoriesAccesory.VERDE, categoriesAccesory.FUSION, categoriesAccesory.HH],
     tamanio: '1°',
     cantidad: 3,
-    estado: 'ok',
+    estado: stateAccesory.OK,
     imagen: '',
     isDeleted: false
   },
@@ -81,10 +97,10 @@ export const accesories: Accesories = [
     id: '6',
     nombre: 'sobrepaso',
     tipo: typesAccesory.AGUA,
-    categoria: ['rojo', 'fusion', 'h-h'],
+    categoria: [categoriesAccesory.ROJO, categoriesAccesory.FUSION, categoriesAccesory.HH],
     tamanio: '1°',
     cantidad: 3,
-    estado: 'ok',
+    estado: stateAccesory.OK,
     imagen: '',
     isDeleted: false
   },
@@ -92,10 +108,10 @@ export const accesories: Accesories = [
     id: '7',
     nombre: 'llave de paso',
     tipo: typesAccesory.AGUA,
-    categoria: ['rojo', 'esferica', 'fusion', 'h-h'],
+    categoria: [categoriesAccesory.ROJO, categoriesAccesory.FUSION, categoriesAccesory.HH],
     tamanio: '1/2',
     cantidad: 3,
-    estado: 'ok',
+    estado: stateAccesory.OK,
     imagen: '',
     isDeleted: false
   },
@@ -103,10 +119,10 @@ export const accesories: Accesories = [
     id: '8',
     nombre: 'codo',
     tipo: typesAccesory.AGUA,
-    categoria: ['rojo', 'rosca', 'h-h'],
+    categoria: [categoriesAccesory.VERDE, categoriesAccesory.FUSION, categoriesAccesory.HH],
     tamanio: '1/2',
     cantidad: 0,
-    estado: 'acondicionar',
+    estado: stateAccesory.REPARAR,
     imagen: '',
     isDeleted: true
   },
@@ -114,10 +130,43 @@ export const accesories: Accesories = [
     id: '9',
     nombre: 'curva',
     tipo: typesAccesory.DESAGUE,
-    categoria: ['h-h'],
+    categoria: [categoriesAccesory.HH],
     tamanio: '110',
     cantidad: 6,
-    estado: 'ok',
+    estado: stateAccesory.OK,
+    imagen: '',
+    isDeleted: false
+  },
+  {
+    id: '10',
+    nombre: 'sobrepaso',
+    tipo: typesAccesory.GAS,
+    categoria: [categoriesAccesory.HH],
+    tamanio: '25',
+    cantidad: 2,
+    estado: stateAccesory.OK,
+    imagen: '',
+    isDeleted: false
+  },
+  {
+    id: '11',
+    nombre: 'briba',
+    tipo: typesAccesory.DESAGUE,
+    categoria: [categoriesAccesory.HH],
+    tamanio: '90',
+    cantidad: 2,
+    estado: stateAccesory.OK,
+    imagen: '',
+    isDeleted: false
+  },
+  {
+    id: '12',
+    nombre: 'pico de loro',
+    tipo: typesAccesory.HERRAMIENTA,
+    categoria: [''],
+    tamanio: '',
+    cantidad: 1,
+    estado: stateAccesory.REPARAR,
     imagen: '',
     isDeleted: false
   },
